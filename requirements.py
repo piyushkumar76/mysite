4c1484b7f26f427639f9b61e9e8893ea38d4f136
Piyush Munjal <piyushmunjal76@gmail.com>
2018-04-09 20:13:14 +0530

    Requiements.py

---
+++ requirements.py
@@ -0,0 +1,8 @@
+from pip import main
+
+package_list = ['django','django-cors-headers','geopy','requests']
+for i in package_list:''
+	main(['install', i])
+#build_essentials
+#python3-dev
+#redis-server
